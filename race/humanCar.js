585824d5acdc498b49f3f8d9afb7f209cda3c33f
Alexandru Jercan <jercan_alex27@yahoo.com>
2022-09-27 21:52:03 +0300

    feat: implemented input

--- race/humanCar.js
+++ race/humanCar.js
@@ -1,20 +1,40 @@
 const defaultKeymap = {
-    "W": (1, 0),
-    "A": (0, -1),
-    "S": (-1, 0),
-    "D": (0, 1),
+    "w": [1, 0],
+    "a": [0, -1],
+    "s": [-1, 0],
+    "d": [0, 1],
 };
 
 export class HumanCar {
-    constructor(document, keyMap=defaultKeymap) {
-        document.addEventListener("keydown", (event) => {
-            const key = event.key;
+    constructor(document, keyMap = defaultKeymap) {
+        this.map = {};
+        this.keyMap = keyMap;
 
-            console.log(key);
+        document.addEventListener("keydown", (event) => {
+            this.map[event.key] = true;
+        });
+
+        document.addEventListener("keyup", (event) => {
+            this.map[event.key] = false;
         });
     }
 
+    getInput(map) {
+        const input = [0, 0];
+
+        for (const key in map) {
+            if (this.map[key] && key in this.keyMap) {
+                const value = this.keyMap[key];
+
+                input[0] += value[0];
+                input[1] += value[1];
+            }
+        }
+
+        return input;
+    }
+
     waitInput() {
-        return 0;
+        return this.getInput(this.map);
     }
 }

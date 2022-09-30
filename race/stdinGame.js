fa6e5971032779bb454e3cf926aeeddd7573eb1d
Alex Jercan <a.jercan@accenture.com>
2022-09-30 13:10:21 +0300

    feat: updated stdinGame

--- race/stdinGame.js
+++ race/stdinGame.js
@@ -2,12 +2,14 @@ import { Point } from "./engine/point.js";
 import { Car } from "./car.js";
 import { Track } from "./track.js";
 import { StdinInput } from "./stdinInput.js";
+import { ModelInput } from "./modelInput.js";
 
 export class StdinGame {
     constructor() {
         this.track = new Track([new Point(100, 100), new Point(200, 500), new Point(500, 500), new Point(700, 100)]);
         this.player = new Car(this.track);
         this.input = new StdinInput();
+        this.modelInput = new ModelInput(this.player);
     }
 
     async update(deltaTime) {
@@ -15,11 +17,6 @@ export class StdinGame {
 
         this.player.move(input[0], input[1], deltaTime);
 
-        console.log({
-            // TODO: Log information for machine learning model to train on
-            position: this.player.position,
-            rotation: this.player.rotation,
-            velocity: this.player.velocity,
-        });
+        console.log(this.modelInput.observations());
     }
 }

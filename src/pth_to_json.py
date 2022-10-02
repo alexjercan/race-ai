0a4296e11b672488e12c0b8651d38d8575fbd655
Alexandru Jercan <jercan_alex27@yahoo.com>
2022-10-02 10:13:06 +0300

    feat: updated pth_to_json script

--- src/pth_to_json.py
+++ src/pth_to_json.py
@@ -8,11 +8,11 @@ from environment import RaceEnv
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--pth", type=str, required=True, dest="pth", help="Path to the pth file")
-    parser.add_argument("--json", type=str, default="model.json", dest="json", help="Path to the json file")
+    parser.add_argument("--js", type=str, default="race/model.js", dest="js", help="Path to the model.js file")
 
     args = parser.parse_args()
     pth_path = args.pth
-    json_path = args.json
+    js_path = args.js
 
     env = RaceEnv()
     model = DQN_RAM(env.observation_space.shape[0], env.action_space.n)
@@ -48,5 +48,7 @@ if __name__ == "__main__":
         },
     ]
 
-    with open(json_path, "w") as f:
-        json.dump(json_model, f)
+    model_file_tempalte = """export const data_layers = '%s';"""
+
+    with open(js_path, "w") as f:
+        f.write(model_file_tempalte % (json.dumps(json_model),))

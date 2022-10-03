10929e008e16972e32978ff8ccdaba22f285ac1b
Alex Jercan <a.jercan@accenture.com>
2022-10-03 15:20:29 +0300

    fix: fixed the borders of the track

--- race/track.js
+++ race/track.js
@@ -4,7 +4,8 @@ import { Point } from "./engine/point.js";
 export const track_waypoints = {
     simple: [new Point(100, 100), new Point(200, 500), new Point(500, 500), new Point(700, 100)],
     medium: [new Point(200, 100), new Point(100, 500), new Point(300, 800), new Point(700, 800), new Point(900, 500), new Point(500, 100)],
-} 
+    hard: [new Point(100, 100), new Point(300, 500), new Point(100, 800), new Point(700, 800), new Point(900, 500), new Point(500, 100)],
+}
 
 export class Track {
     constructor(waypoints, radius=50) {
@@ -21,6 +22,23 @@ export class Track {
     }
 
     initCollider() {
+        function intersection(start1, end1, start2, end2) {
+            const p = start1;
+            const r = new Point(end1.x - p.x, end1.y - p.y);;
+
+            const q = start2;
+            const s = new Point(end2.x - q.x, end2.y - q.y);
+
+            const rxs = r.cross(s);
+
+            const t = new Point(q.x - p.x, q.y - p.y).cross(s) / rxs;
+
+            return new Point(p.x + t * r.x, p.y + t * r.y);
+        }
+
+        const centerX = this.waypoints.map((point) => point.x).reduce((acc, e) => acc + e, 0) / this.waypoints.length;
+        const centerY = this.waypoints.map((point) => point.y).reduce((acc, e) => acc + e, 0) / this.waypoints.length;
+
         this.edgesInner = []
         this.edgesOuter = []
 
@@ -31,14 +49,35 @@ export class Track {
             const waypoint = this.waypoints[i];
             const nextWaypoint = this.waypoints[((i + 1) % n + n) % n];
 
+            const centerVec = new Point(centerX - waypoint.x, centerY - waypoint.y);
+
             const prevVec = new Point(prevWaypoint.x - waypoint.x, prevWaypoint.y - waypoint.y).normalize();
-            const nextVec = new Point(nextWaypoint.x - waypoint.x, nextWaypoint.y - waypoint.y).normalize();
+            const nextVec = new Point(waypoint.x - nextWaypoint.x, waypoint.y - nextWaypoint.y).normalize();
 
-            const prevTranslation = new Point(prevVec.x * this.radius, prevVec.y * this.radius);
-            const nextTranslation = new Point(nextVec.x * this.radius, nextVec.y * this.radius);
+            const pi1 = new Point(-1 * prevVec.y, prevVec.x);
+            const pi2 = new Point(-1 * nextVec.y, nextVec.x);
 
-            this.edgesInner.push(new Point(prevTranslation.x + nextTranslation.x + waypoint.x, prevTranslation.y + nextTranslation.y + waypoint.y));
-            this.edgesOuter.push(new Point(waypoint.x - prevTranslation.x - nextTranslation.x, waypoint.y - prevTranslation.y - nextTranslation.y));
+            const po1 = new Point(-1 * pi1.x, -1 * pi1.y);
+            const po2 = new Point(-1 * pi2.x, -1 * pi2.y);
+
+            this.edgesInner.push(
+                intersection(
+                    new Point(prevWaypoint.x + pi1.x * this.radius, prevWaypoint.y + pi1.y * this.radius),
+                    new Point(waypoint.x + pi1.x * this.radius, waypoint.y + pi1.y * this.radius),
+                    new Point(nextWaypoint.x + pi2.x * this.radius, nextWaypoint.y + pi2.y * this.radius),
+                    new Point(waypoint.x + pi2.x * this.radius, waypoint.y + pi2.y * this.radius),
+                )
+            );
+
+            this.edgesOuter.push(
+                intersection(
+                    new Point(prevWaypoint.x + po1.x * this.radius, prevWaypoint.y + po1.y * this.radius),
+                    new Point(waypoint.x + po1.x * this.radius, waypoint.y + po1.y * this.radius),
+                    new Point(nextWaypoint.x + po2.x * this.radius, nextWaypoint.y + po2.y * this.radius),
+                    new Point(waypoint.x + po2.x * this.radius, waypoint.y + po2.y * this.radius),
+                )
+            );
+
         }
     }
 

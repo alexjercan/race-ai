339090e2f63c634f21fdfebe879ef60b7fa501de
Alex Jercan <a.jercan@accenture.com>
2022-10-03 15:59:32 +0300

    fix: refactor old code

--- race/debug.js
+++ race/debug.js
@@ -1,15 +1,21 @@
 import { TrackRenderer } from "./engine/renderer.js";
-import { ModelInput } from "./modelInput.js";
 
 export class Debug {
     constructor(game) {
-        this.cars = [game.player];
+        this.game = game;
+        this.player = game.player;
+        this.modelInput = game.modelInput;
         this.track = game.track;
 
         this.debugTrack = new TrackRenderer("#00ff00", this.track.waypoints, 1, "butt")
         this.debugTrackInner = new TrackRenderer("#00ff00", this.track.edgesInner, 1, "butt")
         this.debugTrackOuter = new TrackRenderer("#00ff00", this.track.edgesOuter, 1, "butt")
-        this.modelInputs = this.cars.map(car => new ModelInput(car));
+
+        this.fps = 0;
+    }
+
+    update(deltaTime) {
+        this.fps = Math.round(1 / deltaTime);
     }
 
     draw(context) {
@@ -17,43 +23,43 @@ export class Debug {
         this.debugTrackInner.draw(context);
         this.debugTrackOuter.draw(context);
 
-        this.cars.forEach((car) => {
-            const closestPoint = car.getClosestPoint().point;
+        const car = this.player;
+        const closestPoint = car.getClosestPoint().point;
+        context.beginPath();
+        context.moveTo(car.position.x, car.position.y);
+        context.lineTo(closestPoint.x, closestPoint.y);
+        context.lineWidth = 1;
+        context.strokeStyle = "#00ff00";
+        context.stroke();
+
+        const modelInput = this.modelInput;
+        modelInput.rays.forEach((rayEnd) => {
+            const rayEndX = rayEnd.x * Math.cos(modelInput.player.rotation) - rayEnd.y * Math.sin(modelInput.player.rotation);
+            const rayEndY = rayEnd.x * Math.sin(modelInput.player.rotation) + rayEnd.y * Math.cos(modelInput.player.rotation);
 
             context.beginPath();
-            context.moveTo(car.position.x, car.position.y);
-            context.lineTo(closestPoint.x, closestPoint.y);
+            context.moveTo(modelInput.player.position.x, modelInput.player.position.y);
+            context.lineTo(modelInput.player.position.x + rayEndX, modelInput.player.position.y + rayEndY);
+            context.lineWidth = 1;
+            context.strokeStyle = "#ff0000";
+            context.stroke();
+        });
+        const obs = modelInput.observations();
+        modelInput.rays.forEach((rayEnd, index) => {
+
+            const rayEndX = obs[index] * (rayEnd.x * Math.cos(modelInput.player.rotation) - rayEnd.y * Math.sin(modelInput.player.rotation));
+            const rayEndY = obs[index] * (rayEnd.x * Math.sin(modelInput.player.rotation) + rayEnd.y * Math.cos(modelInput.player.rotation));
+
+            context.beginPath();
+            context.moveTo(modelInput.player.position.x, modelInput.player.position.y);
+            context.lineTo(modelInput.player.position.x + rayEndX, modelInput.player.position.y + rayEndY);
             context.lineWidth = 1;
             context.strokeStyle = "#00ff00";
             context.stroke();
         });
 
-        this.modelInputs.forEach((modelInput) => {
-            modelInput.rays.forEach((rayEnd) => {
-                const rayEndX = rayEnd.x * Math.cos(modelInput.player.rotation) - rayEnd.y * Math.sin(modelInput.player.rotation);
-                const rayEndY = rayEnd.x * Math.sin(modelInput.player.rotation) + rayEnd.y * Math.cos(modelInput.player.rotation);
-
-                context.beginPath();
-                context.moveTo(modelInput.player.position.x, modelInput.player.position.y);
-                context.lineTo(modelInput.player.position.x + rayEndX, modelInput.player.position.y + rayEndY);
-                context.lineWidth = 1;
-                context.strokeStyle = "#ff0000";
-                context.stroke();
-            });
-
-            const obs = modelInput.observations();
-            modelInput.rays.forEach((rayEnd, index) => {
-
-                const rayEndX = obs[index] * (rayEnd.x * Math.cos(modelInput.player.rotation) - rayEnd.y * Math.sin(modelInput.player.rotation));
-                const rayEndY = obs[index] * (rayEnd.x * Math.sin(modelInput.player.rotation) + rayEnd.y * Math.cos(modelInput.player.rotation));
-
-                context.beginPath();
-                context.moveTo(modelInput.player.position.x, modelInput.player.position.y);
-                context.lineTo(modelInput.player.position.x + rayEndX, modelInput.player.position.y + rayEndY);
-                context.lineWidth = 1;
-                context.strokeStyle = "#00ff00";
-                context.stroke();
-            });
-        });
+        context.font = '25px Arial';
+        context.fillStyle = 'black';
+        context.fillText("FPS: " + this.fps + " LAPS: " + this.player.laps + " Reward: " + this.game.reward, 10, 30);
     }
 }
--- race/webGame.js
+++ race/webGame.js
@@ -2,18 +2,32 @@ import { Point } from "./engine/point.js";
 import { Car } from "./car.js";
 import { Track, track_waypoints } from "./track.js";
 import { HumanInput } from "./humanInput.js";
+import { PredictInput } from "./predictInput.js";
+import { ModelInput } from "./modelInput.js";
+import { data_layers } from "./model.js";
+import { Model } from "./torch/model.js";
 
 export class WebGame {
     constructor(track_name="simple") {
         this.track = new Track(track_waypoints[track_name]);
+
         this.player = new Car(this.track);
-        this.input = new HumanInput(document);
+        this.humanInput = new HumanInput(document);
+
+        this.modelInput = new ModelInput(this.player);
+        const layers = JSON.parse(data_layers);
+        this.aiInput = new PredictInput(new Model(layers), this.modelInput);
+
+        this.aiMode = false;
+        this.reward = 0;
     }
 
     update(deltaTime) {
-        const input = this.input.waitInput();
+        const input = (this.aiMode) ? this.aiInput.waitInput() : this.humanInput.waitInput();
 
         this.player.move(input[0], input[1], deltaTime);
+
+        this.reward += this.modelInput.reward();
     }
 
     draw(context) {

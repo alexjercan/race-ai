f031117fe280d0b86ee0161a349be924ed7b0bfa
Alexandru Jercan <jercan_alex27@yahoo.com>
2022-10-03 22:43:55 +0300

    feat: added random start to stdin game

--- race/stdinGame.js
+++ race/stdinGame.js
@@ -8,10 +8,14 @@ export class StdinGame {
     constructor(track_name="simple") {
         this.track = new Track(track_waypoints[track_name]);
 
-        const position = new Point(this.track.waypoints[0].x, this.track.waypoints[0].y);
+        const n = this.track.waypoints.length;
+        const startIndex = Math.floor(Math.random() * n);
+        const startIndexNext = (startIndex + 1) % n;
+
+        const position = new Point(this.track.waypoints[startIndex].x, this.track.waypoints[startIndex].y);
         const direction = new Point(
-            this.track.waypoints[1].x - this.track.waypoints[0].x,
-            this.track.waypoints[1].y - this.track.waypoints[0].y
+            this.track.waypoints[startIndexNext].x - this.track.waypoints[startIndex].x,
+            this.track.waypoints[startIndexNext].y - this.track.waypoints[startIndex].y
         )
         const rotation = Math.atan2(direction.x, -1 * direction.y);
 
@@ -19,6 +23,14 @@ export class StdinGame {
         this.input = new StdinInput();
 
         this.env = new AgentEnvironment(this.player, this.track);
+
+        this.env.nextWaypointIndex = startIndexNext;
+
+        const waypoint = this.track.waypoints[startIndex];
+        const proj = this.player.position.getProjectionOnSegment(this.track.waypoints[startIndex], this.track.waypoints[startIndexNext]);
+        const distance = this.track.distances[startIndex] + new Point(proj.x - waypoint.x, proj.y - waypoint.y).magnitude();
+        const totalDistance = this.track.totalDistance;
+        this.env.lastProgress = this.env.laps + distance / totalDistance;
 
         this.input.output({
             observations: this.env.observations(),

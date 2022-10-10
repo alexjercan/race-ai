f56c9073ba9f97e93c76aed0d9308dc35180e19b
Alex Jercan <a.jercan@accenture.com>
2022-10-10 16:05:12 +0300

    feat: added helper function for train mode viz

--- src/pth_to_json.py
+++ src/pth_to_json.py
@@ -1,18 +1,49 @@
+import glob
 import json
 import torch
 import argparse
 
 from model import DQN_RAM
 from environment import RaceEnv
+from pathlib import Path
+
+
+def model_to_json(model: DQN_RAM) -> str:
+    json_model = model.to_json()
+
+    return json.dumps(json_model)
 
 
 def create_model(model: DQN_RAM, js_path="race/model/model.js"):
-    json_model = model.to_json()
+    model_str = model_to_json(model)
 
     model_file_tempalte = """export const data_layers = '%s';"""
 
     with open(js_path, "w") as f:
-        f.write(model_file_tempalte % (json.dumps(json_model),))
+        f.write(model_file_tempalte % (model_str,))
+
+
+def dump_models(js_path="race/model/train.js"):
+    paths = sorted(glob.glob("models/*.pth"))
+
+    env = RaceEnv()
+    model = DQN_RAM(env.observation_space.shape[0], env.action_space.n)
+
+    lines = []
+    for pth_path in paths:
+        model.load_state_dict(torch.load(pth_path))
+
+        model_str = model_to_json(model)
+        model_name = Path(pth_path).stem
+
+        model_template = """{"name": "%s", "layers": '%s'}""" % (model_name, model_str)
+
+        lines.append(model_template)
+
+    content = """export const data_layers = [%s];""" % (", ".join(lines),)
+
+    with open(js_path, "w") as f:
+        f.write(content)
 
 
 if __name__ == "__main__":
@@ -32,9 +63,12 @@ if __name__ == "__main__":
     pth_path = args.pth
     js_path = args.js
 
-    env = RaceEnv()
-    model = DQN_RAM(env.observation_space.shape[0], env.action_space.n)
+    if pth_path == "all":
+        dump_models(js_path)
+    else:
+        env = RaceEnv()
+        model = DQN_RAM(env.observation_space.shape[0], env.action_space.n)
 
-    model.load_state_dict(torch.load(pth_path))
+        model.load_state_dict(torch.load(pth_path))
 
-    create_model(model, js_path)
+        create_model(model, js_path)
